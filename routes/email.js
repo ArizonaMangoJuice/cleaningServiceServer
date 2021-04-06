e7082f28247539d1696e6dc2cd099f5a411b116a
isael lizama <isaelwebdeveloper@gmail.com>
2021-04-06 11:28:42 -0600

    stopped using styles kept it simple

--- routes/email.js
+++ routes/email.js
@@ -18,58 +18,20 @@ transporter.use('compile', inlineCss());
 
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
-  const { firstName, lastName, email } = req.body
+  const { firstName, lastName, email, phone } = req.body
 
   var mailOptions = {
     from: process.env.SERVEREMAIL,
     to: email,
     subject: `${firstName} ${lastName} has an enquiry`,
-    html: `<style>
-    .package-card-container{
-      border: none;
-      display: -webkit-flex;
-      display: flex;
-      -webkit-flex-direction: column;
-      flex-direction: column;
-      background-color: #f8f5f1;
-      width: 21%;
-      margin: 0 2%;
-      height: 360px;
-      text-align: center;
-      border-radius: 5px;
-      transition: .2s;
-      cursor: pointer;
-    }
-    
-    .package-card-title {
-      border-top-left-radius: 5px;
-      border-top-right-radius: 5px;
-      font-weight: 400;
-      padding: 20px 15px;
-      margin: 0;
-      border-bottom: 1px solid rgba(88,119,146,.2);
-    }
-    
-    .package-card-price {
-      font-size: 32px;
-      margin: 15px 0 0;
-      font-weight: 700;
-      transition: .2s;
-    }
-    
-    .packages-cards-container .package-card-terms {
-      font-weight: 300;
-      font-size: 14px;
-    }
-  </style>
-  
-    <div class='package-card-container'>
-      <h3 class="package-card-title ">COMPLETE PACK</h3>
-      <p class="package-card-price ">$215.00</p>
-      <span class="package-card-terms">Per Month</span>
-      <p class="package-card-desc">Extra charges are based on extra work taken.</p>
-    </div>
-  `
+    html: `
+      <div>
+        <h1>Please call ${firstName} ${lastName}</h1>
+        <a href='555-555-5555'> </a>
+        <p>The potential client also has an email </p>
+        <h2>${email}</h2>
+      </div>
+    `
   }
 
   try {
